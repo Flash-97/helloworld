1eeb7c5d786d87af2a941f1db73622b3fa022591
hi <hi@DESKTOP-83UI22K>
2020-05-07 11:18:34 +0900

    2st commit!!!

--- src/com/javaex/helloworld/HelloWorld.java
+++ src/com/javaex/helloworld/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("안녕세상");
+		System.out.println("안녕세상, Hello");
+		System.out.println("Helloworld");
 	}
 
 }
